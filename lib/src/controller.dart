3d3ce90bb854556f748ca48095a0bdc42d1ddc91
Đức Nghĩa <dArkgunz1998@gmail.com>
2020-08-19 09:37:29 +0700

    Update controller.dart

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -232,7 +232,7 @@ class StatefulMapController {
       {bool verbose = false,
       Icon markerIcon = const Icon(Icons.location_on),
       bool noIsolate = false}) async {
-    print("From geojson $data");
+ //   print("From geojson $data");
 
     final geojson = GeoJson();
     geojson.processedFeatures.listen((GeoJsonFeature feature) {
